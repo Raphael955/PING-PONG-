f3101ccd86e9adecba1a6377463aafa983a921a3
Raphael955 <103196210+Raphael955@users.noreply.github.com>
2022-04-11 16:55:02 +0200

    Add files via upload

---
+++ pac man.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+pac_man = "pos_x" : 50 , 
+          "pos_y" : 250,
+          "vies"  : 3  ,
+          "style" : "normal"
+          
+          "vitesse" 5
+          "direction" : "bas","haut","gauche","droite"
+          
+          
+liste_fantome = []
+fantome1= "pos_x"
+          "pos_y"
+          "vitesse" 4,5
+          "direction" : "bas","haut","gauche","droite"
+          "vies" : 1
+fantome2= "pos_x"
+          "pos_y"
+          "vitesse" 4,5
+          "direction" : "bas","haut","gauche","droite"
+          "vies" : 1
+fantome3= "pos_x"
+          "pos_y"
+          "vitesse" 4,5
+          "direction" : "bas","haut","gauche","droite"
+          "vies" : 1
+
+laby = [[0,0,0,0,0,0]]          
+          
+         
+          
